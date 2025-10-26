9f18c1ba59eab4c840920d00d90d4e9d2b986c1a
Jonathan Gabriel Silva <jonathan.gmsilva@gmail.com>
2025-10-26 08:29:19 -0300

    Finalizado desafio

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -10,6 +10,8 @@ public class AppDbContext : DbContext
     public DbSet<Order> Orders { get; set; } = null!;
     public DbSet<OrderLine> OrderLines { get; set; } = null!;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlite();
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
 }
